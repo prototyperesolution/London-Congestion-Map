cda588ebfa2b01bce6051f8870d52117eb085c30
Joe <86804949+prototyperesolution@users.noreply.github.com>
2024-08-08 21:34:22 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,4 +7,6 @@ def process_active_stops():
     return df
 
 def return_stops_in_bounds(df, bounds):
+    min_lat, min_long = bounds['_southWest']['lat'], bounds['_southWest']['lng']
+    max_lat, max_long = bounds['_northEast']['lat'], bounds['_northEast']['lng']
     
