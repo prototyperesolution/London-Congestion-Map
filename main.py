fa4d509ebc4b62f164f1bc9a3893a2f56ab855c8
Joe <86804949+prototyperesolution@users.noreply.github.com>
2024-08-10 21:03:06 +0100

    maps of tube station

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import folium
 from folium.plugins import HeatMap
 import preprocessing
 from streamlit_folium import st_folium
-
-stops_data = preprocessing.process_active_stops()
+import math
 
 STAMEN_TONER_ATTRIBUTION = (
     'Map tiles by <a href="http://stamen.com">Stamen Design</a>, '
@@ -24,7 +23,7 @@ def fetch_crowding_data(Naptan):
 def create_map():
     # Center the map on London
     london_coords = [51.5074, -0.1278]
-    m = folium.Map(location=london_coords, zoom_start=10, 
+    m = folium.Map(location=london_coords, zoom_start=100, 
                    tiles="CartoDB Positron", attr="Stamen Terrain")
     
     return m
@@ -34,9 +33,16 @@ def create_map():
 
 def main():
     st.title("Live map boundaries in London")
+    stops_data = preprocessing.process_active_stops()
     col1, col2 = st.columns([3,1])
     with col1:
         folium_map = create_map()
+        for index, stop in stops_data.iterrows():
+            if math.isnan(stop['Latitude']) or math.isnan(stop['Longitude']):
+                st.write(stop)
+            else:
+                folium.Marker(location = [stop['Latitude'], stop['Longitude']],
+                              popup = stop['CommonName']).add_to(folium_map)
         map_state = st_folium(folium_map, width=700, height=500)
        
     with col2:
@@ -44,15 +50,15 @@ def main():
             st.session_state['map_bounds'] = None
 
         # Add a button to capture and display the map bounds
-        if st.button("Show Current Bounds"):
+        if st.button("Show Count of Current Stations"):
             if map_state and 'bounds' in map_state:
                 st.session_state['map_bounds'] = map_state['bounds']
 
         # Display the map bounds when the button is clicked
         if st.session_state['map_bounds']:
             bounds = st.session_state['map_bounds']
-            st.write(f"**North-East Bound:** {bounds['_northEast']}")
-            st.write(f"**South-West Bound:** {bounds['_southWest']}")
+            curr_stations = preprocessing.return_stops_in_bounds(stops_data, bounds)
+            st.write(len(curr_stations))
         else:
             st.write("Click the button to show the current map bounds.")
                               
--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,16 @@
 import pandas as pd
-import requests
+import math
 
 def process_active_stops():
     df = pd.read_csv("data/Stops.csv", low_memory = False)
-    df = df[(df["ParentLocalityName"] == "London") & (df["Status"] == "active")]
+    df = df.dropna(subset=['Latitude', 'Longitude', 'NaptanCode'])
+    df = df[(df["ParentLocalityName"] == "London") & (df["Status"] == "active") &
+            (df["ATCOCode"].str.startswith("9"))]
     return df
 
 def return_stops_in_bounds(df, bounds):
     min_lat, min_long = bounds['_southWest']['lat'], bounds['_southWest']['lng']
     max_lat, max_long = bounds['_northEast']['lat'], bounds['_northEast']['lng']
-    
+
+    stations_in_frame = df[(df["Longitude"] < max_long) & (min_long < df["Longitude"]) & (min_lat < df["Latitude"]) &(df["Latitude"] < max_lat)]
+    return stations_in_frame
